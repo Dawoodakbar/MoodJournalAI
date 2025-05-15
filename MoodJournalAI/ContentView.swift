e80694d699bc5d2bb0d0c778710756894779b960
Dawood Akbar <uzumakiminato806@gmail.com>
2025-05-15 22:35:25 +0500

    initial commit

--- MoodJournalAI/ContentView.swift
+++ MoodJournalAI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, AI Journal!")
+            Text("Hello, AI Journal App!")
         }
         .padding()
     }
