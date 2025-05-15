5b2626c21946b156045effd1581f55b49a192ad6
Dawood Akbar <uzumakiminato806@gmail.com>
2025-05-15 22:09:24 +0500

    Initial Commit

---
+++ MoodJournalAI/MoodJournalAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodJournalAIApp.swift
+//  MoodJournalAI
+//
+//  Created by Dawood Akbar on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodJournalAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
